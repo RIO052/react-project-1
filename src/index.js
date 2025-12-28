52c1dbc72552b00ba73e06550efb0ce4ac0a2dfe
RIO052 <sayantan196bhadury@gmail.com>
2025-12-28 17:00:46 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import BMIcalculator from './BMIcalculator';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BMIcalculator />
   </React.StrictMode>
 );
 
